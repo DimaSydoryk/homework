cd7058d4850e8e26bf38a6a49097e73938ac0a56
Dmytro <progamer.pg25@gmail.com>
2018-11-20 14:32:16 +0200

    реалізація классу

---
+++ gazonokosarka.cpp
@@ -0,0 +1,29 @@
+#include "gazonokosarka.h"
+#include <iostream>
+using namespace std;
+
+
+void gazonokosarka::getShurunaSkosyTravu()
+{
+	cout << "Enter the ShurunaSkosyTravu(in santimitres): ";
+	cin >> shurunatravu;
+	cout << "ShurunaTravu: " << shurunatravu << endl;
+}
+
+void gazonokosarka::getPowerOfEngine()
+{	cout << "Enter the power(in wats): ";
+	cin >> power;
+	cout << "power: " << pwoer << endl;
+}
+void gazonokosarka::getModel()
+{   cout << "Enter the Model: ";
+    cin >> model;
+	cout << "Model: " << model << endl;
+void gazonokosarka::getMaterial()
+{
+	cout << "Enter ObyemBaku: ";
+	cin >> obyembaku;
+	cout << "ObyemBaku: " << obyembaku << endl;
+}
+
+
