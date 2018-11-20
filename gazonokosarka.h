c9b4d4e71caaa04e47e53095d503ded9e6518494
Dmytro <progamer.pg25@gmail.com>
2018-11-20 14:28:35 +0200

    створення класу Газонокосарка

---
+++ gazonokosarka.h
@@ -0,0 +1,21 @@
+#ifndef gazonokosarka_H
+#define gazonokosarka_H
+#include <string>
+class gazonokosarka
+{
+private:
+	double ShurunaTravu, PowerOfEngine, ObyemBaku;
+	char Model;
+public:
+	void getShurunaTravu();
+	void getWPowerOfEngine();
+	void getModel();
+	void getObyemBaku();
+protected:
+
+protected:
+
+};
+
+#endif
+
